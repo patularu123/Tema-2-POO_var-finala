fe097c2450627a7fb6f7f3982b657456abcd3d4c
patularu123 <61266127+patularu123@users.noreply.github.com>
2020-05-31 12:59:30 +0300

    Add files via upload

---
+++ Nod_prioritate.h.cpp
@@ -0,0 +1,11 @@
+#include "Nod_prioritate.h.h"
+
+Nod_prioritate.h::Nod_prioritate.h()
+{
+    //ctor
+}
+
+Nod_prioritate.h::~Nod_prioritate.h()
+{
+    //dtor
+}
---
+++ Nod_prioritate.h.h
@@ -0,0 +1,16 @@
+#ifndef NOD_PRIORITATE_H_H
+#define NOD_PRIORITATE_H_H
+
+
+class Nod_prioritate.h
+{
+    public:
+        Nod_prioritate.h();
+        virtual ~Nod_prioritate.h();
+
+    protected:
+
+    private:
+};
+
+#endif // NOD_PRIORITATE_H_H
